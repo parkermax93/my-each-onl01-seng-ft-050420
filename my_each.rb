9f4f99e117715d24c629b73b3f33cc66265820cd
Parker Williamson <parker.williamson@gmail.com>
2020-05-06 19:31:40 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,8 @@
-collection = [1, 2, 3, 4]
-
 def my_each(array)
   i = 0 
   while i < array.length
     yield(array[i])
-    i = i + i 
+    i += 1 
   end
+  array 
 end
-my_each(list) {|i| puts "#{i}"}
